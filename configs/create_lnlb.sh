510709776c76a6680ebd5e3b079efe531eb60da4
Joao Paiva <jgpaiva@gsd.inesc-id.pt>
2013-04-22 12:17:43 +0100

    update

--- configs/create_lnlb.sh
+++ configs/create_lnlb.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+mkdir -p lnlb_preemptive
+
 for slice in 0.9 0.7 0.5 0.1 0.01 0.001
 do
 for aboveavg in 0.9 0.7 0.5 0.1 0.01 0.001
     do
         slice_cut=`echo "$slice" | grep --only-matching -P "(?<=0.).*"`
         aboveavg_cut=`echo "$aboveavg" | grep --only-matching -P "(?<=0.).*"`
-        cat cycle_lnlb_preemptive_template.txt | sed "s/SLICE/$slice/" | sed "s/ABOVEAVG/$aboveavg/" > cycle_lnlb_preemptive/lnlb_preemp_${slice_cut}_${aboveavg_cut}
+        cat lnlb_preemptive_template.txt | sed "s/SLICE/$slice/" | sed "s/ABOVEAVG/$aboveavg/" > lnlb_preemptive/lnlb_preemp_${slice_cut}_${aboveavg_cut}
     done
 done
