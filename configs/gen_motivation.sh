b722c4fa207433604ba8eef8dcc4a03f66fd38c5
Joao Paiva <jgpaiva@gsd.inesc-id.pt>
2013-04-23 19:54:37 +0100

    moved getres.sh to results.sh

--- configs/gen_motivation.sh
+++ configs/gen_motivation.sh
@@ -4,7 +4,7 @@ OUTFOLDER=gen/motivation
 rm -r $OUTFOLDER
 mkdir -p $OUTFOLDER
 
-for SIZE in 1000 2000 4000 8000 16000 32000 64000 120000
+for SIZE in 2000 4000 8000 16000 32000 64000 128000 256000 512000 1024000
 do
     echo creating $SIZE
     cat motivation_chord_template.txt | sed "s/SIZETOKEN/$SIZE/" > $OUTFOLDER/chord_$SIZE
---
+++ configs/results_repl.sh
@@ -0,0 +1,7 @@
+for i in `ls runs` 
+do 
+    i="runs/$i"
+    RES1=`cat $i/control.queries | grep --only-matching -P "50 Per.*" | grep --only-matching -P "(?<=\: )..?.?.?.?.?" 2>/dev/null`
+    RES2=`cat $i/control.messagecost.weighted| grep "\d" | grep -P --only-matching "(?<=,)\d+" | j_sum 2>/dev/null`
+    echo "$i,$RES1,$RES2"
+done
