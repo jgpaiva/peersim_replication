ee5d9e75839f3633556d768061c3bca495f6423f
Joao Paiva <jgpaiva@gsd.inesc-id.pt>
2013-06-17 13:21:15 +0100

    generated a more sane distribution

--- py_src/gen_dist.py
+++ py_src/gen_dist.py
@@ -3,12 +3,12 @@
 from __future__ import print_function
 import numpy
 
-alfa = 2
+alfa = 2.5
 n = 100000
 dist = numpy.random.zipf(alfa,100000)
 s = ""
 
-print("# zipf distribution with","alfa:",alfa,"n:",n)
+print("# zipf distribution with","alfa:",alfa,"n:",n,"max:",max(dist),"total:",sum(dist))
 
 for i in dist:
     print(s + str(i),end="")
