4aba64f6e4dec595fbe16bad8cda22aac002feb8
Joao Paiva <jgpaiva@gsd.inesc-id.pt>
2013-06-17 13:23:42 +0100

    updated code to english

--- py_src/gen_dist.py
+++ py_src/gen_dist.py
@@ -3,12 +3,12 @@
 from __future__ import print_function
 import numpy
 
-alfa = 2.5
+alpha = 2.5
 n = 100000
-dist = numpy.random.zipf(alfa,100000)
+dist = numpy.random.zipf(alpha,100000)
 s = ""
 
-print("# zipf distribution with","alfa:",alfa,"n:",n,"max:",max(dist),"total:",sum(dist))
+print("# zipf distribution with","alpha:",alpha,"n:",n,"max:",max(dist),"total:",sum(dist))
 
 for i in dist:
     print(s + str(i),end="")
